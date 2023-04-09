ed6d647505418e97f90cc5a7055013b75d761245
VictorIshizuka <victorishizuka131@gmail.com>
2023-04-09 18:31:20 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,68 @@
+import { useState, useEffect } from "react";
+type Movie = {
+  titulo: string;
+  avatar: string;
+}
+
 const App = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(()=>{
+    loadMovies();
+  }, [])
+
+  const loadMovies = () =>{
+    fetch('https://api.b7web.com.br/cinema/')
+    .then((response) => {
+      return response.json();
+    })
+    .then((json)=>{
+      setMovies(json);
+    })
+    .catch((e)=>{
+      setLoading(false);
+      setMovies([]);
+      console.error(e);
+    })
+  }
+
+  // const loadMovies = async() =>{
+  //   try{
+  //   setLoading(true);
+  //   let response = await fetch('https://api.b7web.com.br/cinema');
+  //   let json = await response.json();
+  //   setLoading(false);
+  //   setMovies(json);
+  //   } catch(e) {
+  //     setLoading(false);
+  //     setMovies([]);
+  //     console.error(e);
+  //   }
+  // }
+
   return (
-    <div className= "bg-blue-500 text-white flex flex-col text-center md:flex-row md:text-green-400 lg:text-black md:justify-between">
-      Texto bem informativo
-      <button className="bg-green-300 text-black p-2 rounded-xl mt-3 md:ml-3 md:mt-0">Um botão</button>
+    <div>
+      {loading &&
+      <div className="text-6xl">Carregando...</div>
+      }
+      {!loading && movies.length > 0 &&
+      <>
+        <div>Total de Filmes: {movies.length}</div>
+        <div className="grid grid-cols-6 gap-3">
+          {movies.map((item, index)=>(
+            <div key={index}>
+              <img src={item.avatar} className="w-32 block" />
+              {item.titulo}
+            </div>
+          ))}
+        </div>
+      </>
+      }
+
+      {!loading && movies.length === 0 && 
+        <div>Tente novamente mais tarde.</div>
+      }
     </div>
   );
 }
